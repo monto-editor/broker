bf049a6bf20e89ca6ad67d668327171297ef6962
Hans Becker <hansbecker91@gmail.com>
2016-07-12 10:47:26 +0200

    Fixed failing test case. A wrong AST product was used Python and Java services now use the same products in the complex test case (no longer pythonAstProduct, just ast; …) Renamed java helper function in test cases to match python helper functions Extracted global lets from Java block

--- test/BrokerSpec.hs
+++ test/BrokerSpec.hs
@@ -25,48 +25,47 @@ spec :: Spec
 spec = do
 
   let register sid prods deps = B.registerService (RegisterServiceRequest sid "" "" Nothing prods deps)
-      -- Java
+      -- Global
       java = "java" :: Language
+      python = "python" :: Language
       tokens = "tokens" :: Product
       ast = "ast" :: Product
       completions = "completions" :: Product
       errors = "errors" :: Product
       sourceProduct = "source" :: Product
-      javaTypechecker = "javaTypechecker" :: ServiceID
-      javaCodeCompletion = "javaCodeCompletion" :: ServiceID
-      javaParser = "javaParser" :: ServiceID
-      javaTokenizer = "javaTokenizer" :: ServiceID
       sourceService = "source" :: ServiceID
-      javaSource = PDEP.ProductDependency "source" "source" java
-      s1 vid = S.SourceMessage vid "s1" java ""
-      s2 vid = S.SourceMessage vid "s2" java ""
-      s3 vid = S.SourceMessage vid "s3" java ""
-      astMsg vid src = P.ProductMessage vid src javaParser ast java "" (toJSON (0::Int))
-      typMsg vid src = P.ProductMessage vid src javaTypechecker errors java "" (toJSON (0::Int))
-      comMsg vid src = P.ProductMessage vid src javaCodeCompletion completions java "" (toJSON (0::Int))
-      tokMsg vid src = P.ProductMessage vid src javaTokenizer tokens java "" (toJSON (0::Int))
       v1 = VersionID 1
       newVersion' v b = first S.fromList $ B.newVersion v b
       newProduct' v b = first S.fromList $ B.newProduct v b
 
+      -- Java
+      javaTypechecker = "javaTypechecker" :: ServiceID
+      javaCodeCompletion = "javaCodeCompletion" :: ServiceID
+      javaParser = "javaParser" :: ServiceID
+      javaTokenizer = "javaTokenizer" :: ServiceID
+      javaSource = PDEP.ProductDependency "source" "source" java
+      javaS1 vid = S.SourceMessage vid "s1" java ""
+      javaS2 vid = S.SourceMessage vid "s2" java ""
+      javaS3 vid = S.SourceMessage vid "s3" java ""
+      javaAstMsg vid src = P.ProductMessage vid src javaParser ast java "" (toJSON (0::Int))
+      javaTypeMsg vid src = P.ProductMessage vid src javaTypechecker errors java "" (toJSON (0::Int))
+      javaCodeCMsg vid src = P.ProductMessage vid src javaCodeCompletion completions java "" (toJSON (0::Int))
+      javaTokenMsg vid src = P.ProductMessage vid src javaTokenizer tokens java "" (toJSON (0::Int))
 
       -- Python
-      python = "python" :: Language
-      pythonAstProduct = "pythonAst" :: Product
-      pythonCompletionsProduct = "pythonCompletions" :: Product
       pythonS20 i = S.SourceMessage i "s20" python ""
       pythonS21 i = S.SourceMessage i "s21" python ""
       pythonParser = "pythonParser" :: ServiceID
       pythonCodeCompletion = "pythonCodeCompletion" :: ServiceID
       pythonSource = PDEP.ProductDependency "source" "source" python
-      pythonAstMsg vid src = P.ProductMessage vid src pythonParser pythonAstProduct python "" (toJSON (0::Int))
+      pythonAstMsg vid src = P.ProductMessage vid src pythonParser ast python "" (toJSON (0::Int))
       pythonCodeCMsg vid src = P.ProductMessage vid src pythonCodeCompletion completions python "" (toJSON (0::Int))
       pythonSourceMsg vid src = S.SourceMessage vid src python ""
 
   context "Service management" $
 
     it "can handle services of two different languages" $ do
-          let broker = register pythonParser [PD.ProductDescription pythonAstProduct python] [pythonSource]
+          let broker = register pythonParser [PD.ProductDescription ast python] [pythonSource]
                      $ register javaParser [PD.ProductDescription ast java] [javaSource]
                      $ B.empty (Port 5010) (Port 5020)
           void $ flip execStateT broker $ do
@@ -75,8 +74,8 @@ spec = do
               [Request "s20" pythonParser [SourceMessage (pythonS20 v1)]]
 
             -- Arrival of sm of java s1 should only generate request for java for s1
-            B.newVersion (s1 v1) `shouldBe'`
-              [Request "s1" javaParser [SourceMessage (s1 v1)]]
+            B.newVersion (javaS1 v1) `shouldBe'`
+              [Request "s1" javaParser [SourceMessage (javaS1 v1)]]
 
   context "Product dependencies" $ do
 
@@ -89,40 +88,40 @@ spec = do
     it "can manage static server dependencies" $
       void $ flip execStateT broker $ do
 
-        newVersion' (s1 v1) `shouldBe'` S.fromList
-           [ Request "s1" javaParser [SourceMessage (s1 v1)]
-           , Request "s1" javaTokenizer [SourceMessage (s1 v1)]
+        newVersion' (javaS1 v1) `shouldBe'` S.fromList
+           [ Request "s1" javaParser [SourceMessage (javaS1 v1)]
+           , Request "s1" javaTokenizer [SourceMessage (javaS1 v1)]
            ]
 
-        let ast1 = astMsg v1 "s1"
+        let ast1 = javaAstMsg v1 "s1"
         newProduct' ast1 `shouldBe'` S.fromList
-               [ Request "s1" javaCodeCompletion [SourceMessage (s1 v1), ProductMessage ast1]
-           , Request "s1" javaTypechecker [SourceMessage (s1 v1), ProductMessage ast1]
+               [ Request "s1" javaCodeCompletion [SourceMessage (javaS1 v1), ProductMessage ast1]
+           , Request "s1" javaTypechecker [SourceMessage (javaS1 v1), ProductMessage ast1]
            ]
 
-        newProduct' (tokMsg v1 "s1") `shouldBe'` S.fromList []
-        newProduct' (comMsg v1 "s1") `shouldBe'` S.fromList []
-        newProduct' (typMsg v1 "s1") `shouldBe'` S.fromList []
+        newProduct' (javaTokenMsg v1 "s1") `shouldBe'` S.fromList []
+        newProduct' (javaCodeCMsg v1 "s1") `shouldBe'` S.fromList []
+        newProduct' (javaTypeMsg v1 "s1") `shouldBe'` S.fromList []
 
     it "can deal with outdated products" $ do
       _ <- flip execStateT broker $ do
         let currentId = VersionID 42
             outdatedId = VersionID 41
 
-        newVersion' (s1 currentId) `shouldBe'` S.fromList
-           [ Request "s1" javaParser [SourceMessage (s1 currentId)]
-           , Request "s1" javaTokenizer [SourceMessage (s1 currentId)]
+        newVersion' (javaS1 currentId) `shouldBe'` S.fromList
+           [ Request "s1" javaParser [SourceMessage (javaS1 currentId)]
+           , Request "s1" javaTokenizer [SourceMessage (javaS1 currentId)]
            ]
 
         -- outdated product arrives
-        newProduct' (astMsg outdatedId "s1") `shouldBe'` S.fromList []
+        newProduct' (javaAstMsg outdatedId "s1") `shouldBe'` S.fromList []
 
       return ()
 
   context "Dynamic dependencies" $ do
 
     it "creates requests for dynamic dependencies only if they have arrived" $ do
-      let broker = register pythonCodeCompletion [PD.ProductDescription pythonCompletionsProduct python] [PDEP.ProductDependency pythonParser pythonAstProduct python]
+      let broker = register pythonCodeCompletion [PD.ProductDescription completions python] [PDEP.ProductDependency pythonParser ast python]
                  $ B.empty (Port 5010) (Port 5020)
 
       void $ flip execStateT broker $ do
@@ -141,7 +140,7 @@ spec = do
           [Request "s20" pythonCodeCompletion [ProductMessage pythonAstMsgS20, SourceMessage (pythonS20 v1)]]
 
     it "creates requests for new dynamic dependencies instantly, if they are already fulfilled" $ do
-      let broker = register pythonCodeCompletion [PD.ProductDescription pythonCompletionsProduct python] []
+      let broker = register pythonCodeCompletion [PD.ProductDescription completions python] []
                    $ B.empty (Port 5010) (Port 5020)
       void $ flip execStateT broker $ do
 
@@ -154,8 +153,8 @@ spec = do
           Just (Request "s20" pythonCodeCompletion [SourceMessage (pythonSourceMsg v1 "s20")])
 
     it "can track complex dynamic product dependencies" $ do
-      let broker = register pythonCodeCompletion [PD.ProductDescription pythonCompletionsProduct python] [PDEP.ProductDependency pythonParser pythonAstProduct python]
-                 $ register pythonParser [PD.ProductDescription pythonAstProduct python] [pythonSource]
+      let broker = register pythonCodeCompletion [PD.ProductDescription completions python] [PDEP.ProductDependency pythonParser ast python]
+                 $ register pythonParser [PD.ProductDescription ast python] [pythonSource]
                  $ register javaTypechecker [PD.ProductDescription errors java] [javaSource,PDEP.ProductDependency javaParser ast java]
                  $ register javaParser [PD.ProductDescription ast java] [javaSource]
                  $ B.empty (Port 5010) (Port 5020)
@@ -186,8 +185,8 @@ spec = do
         B.newVersion (pythonS21 v1) `shouldBe'`
           [Request "s21" pythonParser [SourceMessage (pythonS21 v1)]]
 
-        B.newDynamicDependency (RD.RegisterDynamicDependencies "s21" pythonParser [DD.DynamicDependency "s20" pythonParser pythonAstProduct python]) `shouldBe'`
         -- Registration of dynamic dependency 'python parser service for s21 depends on ast of s20' should not generate requests
+        B.newDynamicDependency (RD.RegisterDynamicDependencies "s21" pythonParser [DD.DynamicDependency "s20" pythonParser ast python]) `shouldBe'`
           Nothing
 
         -- Arrival of sm of s21 should generate no requests, because ast pm of s20 is also required to generate parser request for s21
@@ -218,8 +217,9 @@ spec = do
         -- After registration of a dynamic dependency, that is already fulfilled, the request for it should be generated immediately
         -- completions of s21 now depends on ast of s21 (via product dependency) and on completions of s20 (via dynamic dependency)
 
-        B.newDynamicDependency (RD.RegisterDynamicDependencies "s21" pythonCodeCompletion [DD.DynamicDependency "s20" pythonCodeCompletion pythonCompletionsProduct python]) `shouldBe'`
-        -- TODO: Registration of dynamic dependency 'code completions service for s21 depends on completions product of s20' should not generate requests
+        -- Registration of dynamic dependency 'code completions service for s21 depends on completions product of s20' should 
+        -- immediately generate request for itself, because all products and sources are already available
+        B.newDynamicDependency (RD.RegisterDynamicDependencies "s21" pythonCodeCompletion [DD.DynamicDependency "s20" pythonCodeCompletion completions python]) `shouldBe'`
           Just (Request "s21" pythonCodeCompletion [ProductMessage pythonAstMsgS21, ProductMessage pythonCodeCMsg20])
 
         --
@@ -235,60 +235,60 @@ spec = do
         --     |                 |                 |
         --   errors <-- DYN -- errors <-- DYN -- errors
         --
-        let ast1s1 = astMsg v1 "s1"
-            ast1s2 = astMsg v1 "s2"
-            ast1s3 = astMsg v1 "s3"
-            typ1s1 = typMsg v1 "s1"
-            typ1s2 = typMsg v1 "s2"
-            typ1s3 = typMsg v1 "s3"
+        let ast1s1 = javaAstMsg v1 "s1"
+            ast1s2 = javaAstMsg v1 "s2"
+            ast1s3 = javaAstMsg v1 "s3"
+            type1s1 = javaTypeMsg v1 "s1"
+            type1s2 = javaTypeMsg v1 "s2"
+            type1s3 = javaTypeMsg v1 "s3"
 
         -- Registration of dynamic dependency 'java typechecker service for s3 depends on errors of s2' should not generate requests
         B.newDynamicDependency (RD.RegisterDynamicDependencies "s3" javaTypechecker [DD.DynamicDependency "s2" javaTypechecker errors java]) `shouldBe'`
           Nothing
 
         -- Arrival of sm of s1 should generate parser request for s1
-        B.newVersion (s1 v1) `shouldBe'`
-          [Request "s1" javaParser [SourceMessage (s1 v1)]]
+        B.newVersion (javaS1 v1) `shouldBe'`
+          [Request "s1" javaParser [SourceMessage (javaS1 v1)]]
 
         -- Arrival of ast pm of s1 should generate typechecker request for s1
         B.newProduct ast1s1 `shouldBe'`
-          [Request "s1" javaTypechecker [ProductMessage ast1s1, SourceMessage (s1 v1)]]
+          [Request "s1" javaTypechecker [ProductMessage ast1s1, SourceMessage (javaS1 v1)]]
 
         -- Arrival of sm of s2 should generate parser request for s2
-        B.newVersion (s2 v1) `shouldBe'`
-          [Request "s2" javaParser [SourceMessage (s2 v1)]]
+        B.newVersion (javaS2 v1) `shouldBe'`
+          [Request "s2" javaParser [SourceMessage (javaS2 v1)]]
 
-        --  Arrival of ast pm of s2 should generate typechecker request for s2
+        -- Arrival of ast pm of s2 should generate typechecker request for s2
         -- note: there is no dyn dep on errors of s2 yet, only on errors of s3
         B.newProduct ast1s2 `shouldBe'`
-          [Request "s2" javaTypechecker [ProductMessage ast1s2, SourceMessage (s2 v1)]]
+          [Request "s2" javaTypechecker [ProductMessage ast1s2, SourceMessage (javaS2 v1)]]
 
         -- Registration of dynamic dependency 'java typechecker service for s2 depends on errors of s1' should not generate requests
         B.newDynamicDependency (RD.RegisterDynamicDependencies "s2" javaTypechecker [DD.DynamicDependency "s1" javaTypechecker errors java]) `shouldBe'`
           Nothing
 
         -- Arrival of errors pm of s1 should generate typechecker request for s2, because of dynamic dependency
-        B.newProduct typ1s1 `shouldBe'`
-          [Request "s2" javaTypechecker [ProductMessage typ1s1, ProductMessage ast1s2, SourceMessage(s2 v1)]]
+        B.newProduct type1s1 `shouldBe'`
+          [Request "s2" javaTypechecker [ProductMessage type1s1, ProductMessage ast1s2, SourceMessage(javaS2 v1)]]
 
         -- Arrival of errors pm of s2 should generate no requests, because product dependencies of errors of s3 are not satiesfied
-        B.newProduct typ1s2 `shouldBe'`
+        B.newProduct type1s2 `shouldBe'`
           []
 
         -- Arrival of sm of s3 should generate parser request for s3
-        B.newVersion (s3 v1) `shouldBe'`
-          [Request "s3" javaParser [SourceMessage (s3 v1)]]
+        B.newVersion (javaS3 v1) `shouldBe'`
+          [Request "s3" javaParser [SourceMessage (javaS3 v1)]]
 
         -- Arrival of ast pm of s3 should generate typechecker request for s3
         B.newProduct ast1s3 `shouldBe'`
-          [Request "s3" javaTypechecker [ProductMessage typ1s2, ProductMessage ast1s3, SourceMessage (s3 v1)]]
+          [Request "s3" javaTypechecker [ProductMessage type1s2, ProductMessage ast1s3, SourceMessage (javaS3 v1)]]
 
         -- Arrival of errors pm of s1 should still generate typechecker request for s2, because of dynamic dependency
-        B.newProduct typ1s1 `shouldBe'`
-          [Request "s2" javaTypechecker [ProductMessage ast1s2, ProductMessage typ1s1, SourceMessage(s2 v1)]]
+        B.newProduct type1s1 `shouldBe'`
+          [Request "s2" javaTypechecker [ProductMessage ast1s2, ProductMessage type1s1, SourceMessage(javaS2 v1)]]
 
         -- Arrival of errors pm of s3 should generate no request, because nothing depends on it
-        B.newProduct typ1s3 `shouldBe'`
+        B.newProduct type1s3 `shouldBe'`
           []
 
   where
