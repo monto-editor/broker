93e2e4c17b091bfc72f94d769c78c30c98157dc2
Hans Becker <hansbecker91@gmail.com>
2016-07-14 15:35:18 +0200

    Stylish-haskelled DGCM

--- src/Monto/DependencyGraphCommandMessages.hs
+++ src/Monto/DependencyGraphCommandMessages.hs
@@ -1,11 +1,11 @@
 module Monto.DependencyGraphCommandMessages where
 
-import           Data.Map             (Map)
-import qualified Data.Map             as M
-import           Data.Set             (Set)
-import qualified Data.Set             as S
+import           Data.Map              (Map)
+import qualified Data.Map              as M
+import           Data.Set              (Set)
+import qualified Data.Set              as S
 
-import           Monto.CommandMessage (CommandMessage)
+import           Monto.CommandMessage  (CommandMessage)
 import           Monto.Types
 
 data DependencyGraphCommandMessages
